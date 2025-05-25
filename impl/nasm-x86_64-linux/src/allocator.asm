6dfe0897b7707b536543204f75de69b207f4a4cc
wentam <wentam42@gmail.com>
2025-05-25 13:21:19 -0400

    memory management

--- impl/nasm-x86_64-linux/src/allocator.asm
+++ impl/nasm-x86_64-linux/src/allocator.asm
@@ -25,8 +25,8 @@
 %define ALLOCATION_LENGTH_OFFSET 8
 %define ALLOCATION_DATA_OFFSET 16
 
-%define CHUNK_SIZE (1024*1024)
-%define SYSCALL_SPILL_SIZE 128*1024 ;; Allocations larger than this will syscall
+%define CHUNK_SIZE (5*1024*1024)
+%define SYSCALL_SPILL_SIZE 512*1024 ;; Allocations larger than this will syscall
 
 %define MAP_ANONYMOUS  0x20
 %define MAP_PRIVATE    0x02
